2a0df33875431e8e958d0fd0bf592bb842f61923
JohnKuehnDesign <111250668+JohnKuehnDesign@users.noreply.github.com>
2025-03-26 13:41:43 -0400

    finished 04
    04 finished but test:remote didn't run (karma command not found). test:local ran as expected

--- src/lessons/04-0-higher-order-functions.js
+++ src/lessons/04-0-higher-order-functions.js
@@ -15,19 +15,35 @@ const bae = {};
 // Return an array of the last n elements of an array. If n is undefined,
 // return just the last element.
 bae.last = function (array, n) {
-  // Your code here
+  if (n === undefined) {
+    return array[array.length -1];
+  } else if (n > 0) {
+    return array.slice(-n);
+  } else {
+    return [];
+  }
 };
 
 // Return an array of the first n elements of an array. If n is undefined,
 // return just the first element.
 bae.first = function (array, n) {
-  // Your code here
+  return n === undefined ? array[0] : array.slice(0,n);
 };
+
 
 // Call iterator(value, key, collection) for each element of collection
 // a collection is an array or object
 bae.each = function (collection, iterator) {
   // Your code here
+  if (Array.isArray(collection)) {
+    for (var i = 0, len = collection.length; i < len; i++ ) {
+      iterator(collection[i], i, collection);
+    }
+  } else {
+    for (var i in collection) {
+      iterator(collection[i], i, collection);
+    }
+  }
 };
 
 // Returns the index at which value can be found in the array, or -1 if value
@@ -35,6 +51,14 @@ bae.each = function (collection, iterator) {
 // use the bae.each function you wrote above instead of a for loop
 bae.indexOf = function (array, target) {
   // Your code here
+  var result = -1;
+
+  bae.each(array, function (item, index) {
+    if (item === target && result === -1) {
+      result = index;
+    }
+  });
+  return result;
 };
 
 // Return all elements of an array that pass a truth test.
@@ -42,17 +66,34 @@ bae.indexOf = function (array, target) {
 // test is a function that returns true or false for each item
 bae.filter = function (collection, test) {
   // Your code here
+  var filtered = [];
+  bae.each(collection, function (item) {
+    if (test(item)) {
+      filtered.push(item);
+    }
+  });
+  return filtered;
 };
 
 // Return all elements of an array that don't pass a truth test.
 bae.reject = function (collection, test) {
   // TIP: see if you can re-use bae.filter() here, without simply
   // copying code in and modifying it
+  return bae.filter(collection, function(item) {
+    return !test(item);
+  });
 };
 
 // Produce a duplicate-free version of the array.
 bae.uniq = function (array) {
   // Your code here
+  var uniqArray = [];
+  bae.each(array, function(item) {
+    if (bae.indexOf(uniqArray, item) === -1) {
+      uniqArray.push(item);
+    }
+  });
+  return uniqArray;
 };
 
 // Return the results of applying an iterator to each element.
@@ -61,6 +102,12 @@ bae.uniq = function (array) {
 // the members, it also maintains an array of results.
 bae.map = function (collection, iterator) {
   // Your code here
+  var mapped = [];
+  bae.each(collection, function (item) {
+    const result = (iterator(item));
+    mapped.push(result);
+  });
+  return mapped;
 };
 
 // Reduces an array or object to a single value by repetitively calling
@@ -80,10 +127,24 @@ bae.map = function (collection, iterator) {
 //   }, 0); // should be 6
 bae.reduce = function (collection, iterator, accumulator) {
   // Your code here
+  bae.each(collection, function (item) {
+    if (accumulator === undefined) {
+      accumulator = item;
+    } else {
+      accumulator = iterator(accumulator, item);
+    } 
+  });
+  return accumulator;
 };
 
 // Determine if the array or object contains a given value (using `===`).
 bae.contains = function (collection, target) {
   // TIP: Many iteration problems can be most easily expressed in
   // terms of reduce().
+  return bae.reduce(collection, function(wasFound, item) {
+    if (wasFound) {
+      return true;
+    }
+    return item === target;
+  }, false);
 };
--- src/webflow/index.js
+++ src/webflow/index.js
@@ -2,17 +2,48 @@
 const listElement = document.querySelector('[wb-dash-el="list"]');
 const radios = document.querySelectorAll('[wb-dash-el="radio"]');
 
+const itemTemplates = Array.from(listElement.cloneNode(true).children);
+
 // Function to calculate the total price of items in the list
-function calculateTotal() {}
+function calculateTotal() {
+  const total = Array.from(listElement.querySelectorAll('[wb-dash-el="price"]'))
+    .map((el) => Number(el.textContent))
+    .reduce((acc, curr) => acc + curr, 0);
+  return total;
+}
 
 // Function to update the total price display
-function updateTotal(total) {}
+function updateTotal(total) {
+  const totalElement = document.querySelector('[wb-dash-el="total"]');
+  totalElement.textContent = total;
+}
 
 // Function to filter items by category
-function filterItemsByCategory(category) {}
+function filterItemsByCategory(category) {
+  return itemTemplates.filter((item) => { 
+    if (category === "All") return true;
+    const itemCategory = item.querySelector(
+      '[wb-dash-el="category"]'
+      ).textContent;
+    return itemCategory === category;
+});
+}
 
 // Function to render items based on the selected category
-function renderItems() {}
+function renderItems() {
+  const selectedCategory = document.querySelector(
+    '[wb-dash-el="radio"]:checked'
+  ).nextElementSibling.textContent;
+
+  listElement.innerHTML = "";
+
+  const filteredItems = filterItemsByCategory(selectedCategory);
+
+  filteredItems.forEach((item) => {
+    listElement.appendChild(item.cloneNode(true));
+  });
+  updateTotal(calculateTotal());
+}
 
 // Add change event listeners to all radio buttons
 radios.forEach((radio) => {
@@ -21,3 +52,4 @@ radios.forEach((radio) => {
 
 // Initial calculation and rendering of total
 updateTotal(calculateTotal());
+
